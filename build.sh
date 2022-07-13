4ca8858339a5bd400c100495a56b5c110f172239
wanna <zhg578@163.com>
2022-07-13 19:37:03 +0800

    update build.sh .support without proxy

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
+# remove origin files
+rm -rf go.mod go.sum vendor/
+# rebuild mod and init vendor
+go mod init mobaxterm && go mod tidy && go mod vendor
 # install fyne-cross
-go install github.com/fyne-io/fyne-cross@latest
+go get github.com/fyne-io/fyne-cross
 # build windows executable file
-sudo ~/go/bin/fyne-cross windows -arch=*
+sudo ~/go/bin/fyne-cross windows -arch=* -env "-mod=vendor"
+# build linux executable file
+sudo ~/go/bin/fyne-cross linux -arch=* -tags "GUI" -env "-mod=vendor"
